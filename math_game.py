301e0f8114eb921e4d81ab00bd9dc4f9914521c3
Alex Drozd <drozdster@gmail.com>
2018-05-20 19:52:35 -0500

    Adds win condition

--- math_game.py
+++ math_game.py
@@ -6,10 +6,15 @@ def getQuestion():
     b = randint(0, 9)
     return "What is {} + {}?: ".format(a, b), a, b
 
+count = 0
 while True:
+    count += 1
     question, a, b = getQuestion()
     answer = input(question)
     if answer == str(a + b):
+        if count > 4:
+            print("You win!")
+            break
         continue
     else:
         print("You are wrong. Because of it, you have been eaten. Good job.")
