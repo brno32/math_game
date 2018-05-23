442f2b9a854c061d2e48f2fc8e73260859b5292e
Alex Drozd <drozdster@gmail.com>
2018-05-22 23:05:36 -0500

    implements rudimentary polling

--- math_game.py
+++ math_game.py
@@ -1,13 +1,13 @@
 import math
 import sys
 import os
-from threading import Thread
+from threading import Thread, Event
 from time import time, sleep
 from random import randint
 
 TIMEOUT_MESSAGE = "You are too slow and stupid. You are dead."
 WRONG_MESSAGE = "You are wrong. Because of it, you have been eaten. Good job."
-VICTORY_MESSAGE = "You can do basic math! Hooray!"
+VICTORY_MESSAGE = "You win! You can do (very) basic math! Hooray!"
 
 
 class Question:
@@ -34,33 +34,45 @@ class Question:
         if self.id == 3:
             return str(self.a // self.b) == input
 
-def main():
+def questions(stopFlag):
     count = 0
     while True:
+        if not timer_thread.is_alive():
+            return
         q = Question()
         answer = input(q.getQuestion())
         if q.checkInput(answer):
             count += 1
             if count > 4:
                 print(VICTORY_MESSAGE)
-                sys.exit(0)
-                return
+                break
             continue
         else:
             print(WRONG_MESSAGE)
-            sys.exit(0)
-            return
+            break
+    if not timer_thread.is_alive():
+        return
+    return
 
 def timer():
     for i in range(15):
         sleep(1)
+        if not game.is_alive():
+            return
+    if game.is_alive():
+        # TODO: kill game thread here
+        pass
     print(TIMEOUT_MESSAGE)
-    sys.exit(0)
     return
 
 if __name__ == "__main__":
+    stopFlag = Event()
+    game = Thread(target=questions, args=(stopFlag,))
     timer_thread = Thread(target=timer)
-    game = Thread(target=main)
     timer_thread.start()
     game.start()
-    sys.exit(0)
+
+    timer_thread.join()
+    game.join()
+
+    print('\nExecution ends')
