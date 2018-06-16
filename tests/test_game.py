11c94392e12ed221728653dbe90c803d5ce0bd4e
Alex Drozd <drozdster@gmail.com>
2018-06-16 00:21:38 -0500

    adds requirements.txt and ci file and pytest folder

---
+++ tests/test_game.py
@@ -0,0 +1,30 @@
+def test_difficulty_select():
+    assert True
+
+
+def test_easy():
+    assert True
+
+
+def test_medium():
+    assert True
+
+
+def test_hard():
+    assert True
+
+
+def test_win():
+    assert True
+
+
+def test_lose():
+    assert True
+
+
+def test_play_again():
+    assert True
+
+
+def test_quit():
+    assert True
